47dd68c11740742815a285d788b2a46170db7c49
Layan Alsubaie <layanalsubie@gmail.com>
2026-04-23 18:49:59 +0300

    create flutter project + add cleany

---
+++ vocado/lib/core/navigation/routers.dart
@@ -0,0 +1,8 @@
+class Routes {
+  Routes._();
+  static const String splash = '/';
+  static const String auth = '/auth';
+  static const String taskCreator = '/taskCreator';
+  static const String askViewer = '/askViewer';
+  static const String taskViewer = '/taskViewer';
+}
